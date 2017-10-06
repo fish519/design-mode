10ca0f8c05a2b41b0655ec353dfe745ba712466e
fish519 <744646048@qq.com>
2017-10-06 19:52:50 +0800

    Update 门面模式.js

--- 门面模式.js
+++ 门面模式.js
@@ -1,8 +1,24 @@
-function myForEach(arr,fn) {
-    for(var i = 0; i < arr.length; i++){
-        fn.call(this,arr[i],i)
-    }
+/**
+ * 门面模式
+ * 提供一个高层接口，便于访问
+ */
+function A() {
+    return 100;
 }
-myForEach([1,2,3], function (item, index) {
-    console.log(++item)
-})
+function B() {
+    return 200;
+}
+function getAll() {
+    return A() + B();
+}
+
+console.log(getAll())
+
+
+//一个更加常见的例子
+
+var stopEvent = function( e ){   //同时阻止事件默认行为和冒泡
+  e.stopPropagation();
+  e.preventDefault();
+}
+ 
